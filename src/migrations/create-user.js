088842e10df6c419c5a8929a0e39374000df8a02
I99900K <truongvans1010@gmail.com>
2022-10-12 16:22:57 +0700

    fix some models

--- src/migrations/create-user.js
+++ src/migrations/create-user.js
@@ -1,48 +1,54 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       gender: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
-      typeRole: {
-        type: Sequelize.STRING
+      roleId: {
+        type: Sequelize.STRING,
       },
-      keyRole: {
-        type: Sequelize.STRING
+      phoneNumber: {
+        type: Sequelize.STRING,
+      },
+      positionId: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable("Users");
+  },
 };
--- src/models/user.js
+++ src/models/user.js
@@ -11,13 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            // User.belongsTo(models.Allcode, { foreignKey: 'positionId', targetKey: 'keyMap', as: 'positionData' });
-            // User.belongsTo(models.Allcode, { foreignKey: 'gender', targetKey: 'keyMap', as: 'genderData' });
-            // User.hasOne(models.Markdown, { foreignKey: 'doctorId' });
-            // User.hasOne(models.Doctor_Infor, { foreignKey: 'doctorId' });
-
-            // User.hasMany(models.Schedule, { foreignKey: 'id', targetKey: 'doctorId', as: 'doctorData' });
-            // User.hasMany(models.Booking, { foreignKey: 'patientId', as: 'patientData' });
+            
 
         }
     };
@@ -27,12 +21,11 @@ module.exports = (sequelize, DataTypes) => {
         firstName: DataTypes.STRING,
         lastName: DataTypes.STRING,
         address: DataTypes.STRING,
-        //phoneNumber: DataTypes.STRING,
         gender: DataTypes.STRING,
-        //image: DataTypes.STRING,
-        typeRole: DataTypes.STRING,
-        keyRole: DataTypes.STRING,
-        //positionId: DataTypes.STRING,
+        roleId: DataTypes.STRING,
+        phoneNumber: DataTypes.STRING,
+        positionId: DataTypes.STRING,
+        image: DataTypes.STRING
     }, {
         sequelize,
         modelName: 'User',
