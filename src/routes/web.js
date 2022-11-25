1b7695caa78957f90c921dd0c4860abed2fc0b2e
I99900K <truongvans1010@gmail.com>
2022-11-25 15:59:50 +0700

    update crud user

--- src/routes/web.js
+++ src/routes/web.js
@@ -20,6 +20,8 @@ let initWebRoutes = (app) => {
   router.post("/api/create-new-user", userController.handleCreateNewUser);
   router.delete("/api/delete-user", userController.handleDeleteUser);
   router.put("/api/edit-user", userController.handleEditUser);
+  router.get('/api/allcode', userController.getAllCode)
+
 
   return app.use("/", router);
 };
