54a245506645bab941f8ddf294abded8550f703a
I99900K <truongvans1010@gmail.com>
2022-11-29 13:17:26 +0700

    refactor code

---
+++ server/api/controllers/Patient/SearchDoctor.js
@@ -0,0 +1,36 @@
+const Doctors = require('../../../models/Doctor')
+
+const findNearestDoctors = async (req, res, next) => {
+    const { lng, lat } = req.body
+    //console.log('object ', req.body);
+    try {
+        const doctors = await Doctors.find(
+            {
+                location: {
+                    $near: {
+                        $geometry: {
+                            type: "Point",
+                            coordinates: [lng, lat]
+                        },
+                        maxDistance: 10000,
+                        distanceField: 'distance'
+                    }
+                }
+            })
+        console.log("res", doctors);
+
+        res.send(doctors)
+
+    } catch (error) {
+        if (error) {
+            console.log(error.message)
+            next(error)
+        }
+    }
+}
+
+
+
+module.exports = {
+    findNearestDoctors
+}
